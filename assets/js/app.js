f193581acd888bd7cf870d03d16f480fd6fd107f
Teodor <preprint@pensoft.net>
2023-11-14 10:27:32 +0200

    prod changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -276,7 +276,7 @@ $(document).ready(function() {
         }
     });
 
-    $('a[href^="#"]').click(function() {
+    $('.hackathon a[href^="#"]').click(function() {
       var target = $(this.hash);
       if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
       if (target.length == 0) target = $('html');
