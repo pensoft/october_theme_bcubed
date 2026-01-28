4613d2fa30205992dc4526a2c51f2cc0573e89ce
root <preprint@pensoft.net>
2026-01-28 09:58:31 +0200

    homepage popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -369,6 +369,42 @@ $(document).ready(function() {
 
 });
 
+
+
+
+function openJoinModalBtn(){
+    if (getCookie('joinModalClosed')) {
+        return; // Don't open if cookie is set
+    }
+
+    setTimeout(function() {
+        $(".openJoinModalBtn").trigger("click");
+    }, 10);
+}
+
+// Cookie helper functions (add these if not already present)
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+
 function showHideWP(target){
     // if (width < 1024) {
     // $('.work_packages_container .wp_content').hide();
